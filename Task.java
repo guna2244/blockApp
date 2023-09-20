69fe2eb27bffa1defb187285d8a42c38306e0d2d
guna2244 <138886024+guna2244@users.noreply.github.com>
2023-09-20 14:43:49 +0530

    this is my first commit

---
+++ Task.java
@@ -0,0 +1,17 @@
+package com.jsp.task;
+
+public class Task {
+ public static void main(String[] args) {
+	 
+	
+	 int num=123,rev=0,rem;
+	 while(num>0)
+	 {
+	 rem=num%10;
+	 num=num/10;
+	 rev=rev*10+rem;
+	 }
+	 System.out.println("Reverse of num is : "+rev);
+ }
+}
+	
